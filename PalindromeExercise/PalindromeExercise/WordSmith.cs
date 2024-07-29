e8c6504e1606c659ca7636426afd9cbe14aae2dd
“Noah <urbannoah@yahoo.com>
2024-07-29 08:56:20 -0400

    palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool Palindrome(string str)
+		{
+            var newS = new string(str.Where(x => char.IsLetter(x)).ToArray()).ToLower();
+
+            return newS == new string(newS.Reverse().ToArray());
+        }
+	}
+}
+
